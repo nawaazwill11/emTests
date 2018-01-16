a527e722f952ede3b404feb04d266eefc767699b
nawaazwill11 <mastermindjim@gmail.com>
2018-01-16 16:23:05 +0530

    MAny changes

---
+++ Design/js/custom.js
@@ -0,0 +1,7 @@
+function leftOpenNav() {
+    document.getElementById("leftSidenav").style.width = "295px";
+}
+
+function leftCloseNav() {
+    document.getElementById("leftSidenav").style.width = "0";
+}
