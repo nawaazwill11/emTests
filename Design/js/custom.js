7d5e50793b02b424a75877ce9ba26cf36a440381
nawaazwill11 <mastermindjim@gmail.com>
2018-01-17 09:47:49 +0530

    Added right sidebar. not responsive yet. planned for later.

--- Design/js/custom.js
+++ Design/js/custom.js
@@ -5,3 +5,11 @@ function leftOpenNav() {
 function leftCloseNav() {
     document.getElementById("leftSidenav").style.width = "0";
 }
+
+function rightOpenNav() {
+    document.getElementById("rightSidenav").style.width = "295px";
+}
+
+function rightCloseNav() {
+    document.getElementById("rightSidenav").style.width = "0";
+}
