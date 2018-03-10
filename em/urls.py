3a7e7610dcecf72be9119d831ea8917d0a3fb3d4
nawaazwill11 <mastermindjim@gmail.com>
2018-03-10 22:31:17 +0530

    django unchanged

---
+++ em/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import url
+from em import views
+
+urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
+    url(r'^about/$', views.about, name = 'about'),
+    url(r'^album/$', views.album, name = 'album'),
+    url(r'^contribute/$', views.contribute, name = 'contribute'),
+    url(r'^emergency/$', views.emergency, name = 'emergency'),
+    url(r'^emergency_request/$', views.emergency_request, name = 'emergency_request'),
+    url(r'^event_main/$', views.event_main, name = 'event_main'),
+    url(r'^feedback/$', views.feedback, name = 'feedback'),
+    url(r'^friends/$', views.friends, name = 'friends'),
+    url(r'^myevent/$', views.myevent, name = 'myevent'),
+    url(r'^mytrip/$', views.mytrip, name = 'mytrip'),
+    url(r'^plan_event/$', views.plan_event, name = 'plan_event'),
+    url(r'^plan_trip/$', views.plan_trip, name = 'plan_trip'),
+    url(r'^search_event/$', views.search_event, name = 'search_event'),
+    url(r'^search_trip/$', views.search_trip, name = 'search_trip'),
+    url(r'^settings/$', views.setting, name = 'setting'),
+    url(r'^story/$', views.story, name = 'story'),
+    url(r'^timeline/$', views.timeline, name = 'timeline'),
+    url(r'^traveller_main/$', views.traveller_main, name = 'traveller_main'),
+]
---
+++ em/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+def index(request):
+	return render(request, 'index.html', context=None)
+
+def about(request):
+	return render(request, 'about.html', context=None)
+
+def album(request):
+	return render(request, 'album.html', context=None)
+
+def contribute(request):
+	return render(request, 'contribute.html', context=None)
+
+def emergency(request):
+	return render(request, 'emergency.html', context=None)
+
+def emergency_request(request):
+	return render(request, 'emergency_requests.html', context=None)
+
+def event_main(request):
+	return render(request, 'event_main.html', context=None)
+
+def feedback(request):
+	return render(request, 'feedback.html', context=None)
+
+def friends(request):
+	return render(request, 'friends.html', context=None)
+
+def myevent(request):
+	return render(request, 'myevent.html', context=None)
+
+def mytrip(request):
+	return render(request, 'mytrip.html', context=None)
+
+def plan_event(request):
+	return render(request, 'plan_event.html', context=None)
+
+def plan_trip(request):
+	return render(request, 'plan_trip.html', context=None)
+
+def search_trip(request):
+	return render(request, 'search_trip.html', context=None)
+
+def search_event(request):
+	return render(request, 'search_event.html', context=None)
+
+def setting(request):
+	return render(request, 'settings.html', context=None)
+
+def story(request):
+	return render(request, 'story.html', context=None)
+
+def timeline(request):
+	return render(request, 'timeline.html', context=None)
+
+def traveller_main(request):
+	return render(request, 'traveller_main.html', context=None)
