1ab6b452c82ad4e0fa722c66a8d7b366d63f1e9d
nawaazwill11 <mastermindjim@gmail.com>
2018-04-03 14:33:28 +0530

    plan 2 work-in-progress. total time calculated with pitstop

--- em/mods/test2.py
+++ em/mods/test2.py
@@ -1,16 +1,11 @@
-import datetime
-import time
-from datetime import datetime as dt
-date = datetime.date.today()
+n = 5456
+hours = 0
+mins = 0
 
-time = time.strftime('%H:%M:%S', time.gmtime(12345))
-doj = dob = (str(date)+' '+str(time))
-print(dob,doj)
-datetime_object = dt.strptime('2018-02-31', '%Y-%M-%S')
-time_obj = dt.strptime('13:49:15', '%H:%M:%S')
-#datetime.datetime.combine(datetime.date(date),datetime.time(time))
-day = 31
-month = '03'
-year = 2018
-date = dt(year=year, month=3, day=day)
-print (date)
+while(n > 0):
+	if not (n-60 < 0):
+		n = n - 60
+		hours += 1
+	else:
+		break
+print(hours, n)
